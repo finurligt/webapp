1c3c04c417c558e62da61254feec941ad99f2d11
Fabian Sundholm <fabian.sundholn@hotmail.com>
2021-07-13 12:35:59 +0200

    make leagues work, add non functional add league button

--- src/pages/RatingLite.js
+++ src/pages/RatingLite.js
@@ -23,22 +23,25 @@ export class RatingLite extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            showAddLeagueElement: false,
             league: undefined,
             winner: undefined,
             loser: undefined,
-            games: []
+            games: undefined,
+            leagues: []
         }
 
         this.submitGame = this.submitGame.bind(this);
         this.updateWinner = this.updateWinner.bind(this);
         this.updateLoser = this.updateLoser.bind(this);
+        this.updateLeague = this.updateLeague.bind(this);
 
     }
 
     submitGame(e) {
         e.preventDefault();
 
-        firebase.database().ref('ratingLite/'+this.state.league).push({
+        firebase.database().ref('ratingLite/games/' + this.state.league).push({
             winner: this.state.winner,
             loser: this.state.loser,
             timestamp: firebase.database.ServerValue.TIMESTAMP,
@@ -58,38 +61,48 @@ export class RatingLite extends Component {
         })
     }
 
+    updateLeague(e) {
+        this.setState({
+            league: e.target.value
+        })
+    }
+
     componentDidMount() {
         var dbRef = db.ref('ratingLite');
         dbRef.on('value', (snapshot) => {
             const data = snapshot.val();
             console.log(data)
-            
-            
+
+
             this.setState({
-                games: Object.values(data)
+                games: data.games,
+                leagues: data.leagues
             })
         });
     }
 
     render() {
-        let games = this.state.games;
+        console.log(this.state)
+        let games = this.state.league && this.state.games && this.state.games[this.state.league] ? Object.values(this.state.games[this.state.league]) : [];
 
         let map = {}
         games.forEach((game) => {
             let winnerElo = map[game.winner] ? map[game.winner] : 1200;
             let loserElo = map[game.loser] ? map[game.loser] : 1200;
 
-            var expectedA = 1 / (1 + Math.pow(10,(loserElo-winnerElo)/400));
+            var expectedA = 1 / (1 + Math.pow(10, (loserElo - winnerElo) / 400));
 
             var kValue = 40;
-            var ratingChange = Math.round(kValue*(1-expectedA));
+            var ratingChange = Math.round(kValue * (1 - expectedA));
 
-            map[game.winner]=winnerElo+ratingChange;
-            map[game.loser]=loserElo-ratingChange;
+            map[game.winner] = winnerElo + ratingChange;
+            map[game.loser] = loserElo - ratingChange;
         })
-        let players = Object.entries(map).sort((a,b) => b[1]-a[1]);
+        let players = Object.entries(map).sort((a, b) => b[1] - a[1]);
+        console.log(games)
+        console.log(players)
 
-        
+
 
         return (
             <>
@@ -99,12 +112,21 @@ export class RatingLite extends Component {
                     <div className="col-sm-3" style={{}}></div>
 
                     <div className="col-sm-6" style={{ textAlign: "left", backgroundColor: "white" }}>
-                        <select class="form-select" aria-label="Default select example" style={{marginTop:"20px"}}>
-                            <option selected>Open this select menu</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
+                        {/* Select League */}
+                        <select onChange={this.updateLeague} value={this.state.league} className="form-select" aria-label="Default select example" style={{ marginTop: "20px" }}>
+                            <option defaultValue={undefined}>Select a league...</option>
+                            {Object.values(this.state.leagues).map(league => (
+                                <option key={league.name} value={league.name}>{league.name}</option>
+                            ))}
                         </select>
+                        {this.state.showAddLeagueElement ? null : <button type="submit" className="ml-1 btn btn-primary btn-sm">Add new league</button>}
+
+                        <div className="input-group mt-3">
+                            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                            <div className="input-group-append">
+                                <button className="btn btn-outline-secondary" type="button">Add</button>
+                            </div>
+                        </div>
                         <form className="gameForm" onSubmit={this.submitGame}>
                             <div className="form-group">
                                 <label htmlFor="inputWinner">Winner</label>
@@ -119,16 +141,16 @@ export class RatingLite extends Component {
 
                             <button type="submit" className="btn btn-primary">Submit</button>
                         </form>
-                        
-                        <div className="gameList" style={{ textAlign: "center"}}>
+
+                        <div className="gameList" style={{ textAlign: "center" }}>
                             <h2 >Players</h2>
                             {players.map(player => (
-                                <div >
+                                <div key={player[0]}>
                                     <div>
                                         <div className="row">
 
-                                            <div className="col-6" style={{textAlign:"left"}} >{player[0]}</div>
-                                            <div className="col-6" style={{textAlign:"right"}} >{player[1]}</div>
+                                            <div className="col-6" style={{ textAlign: "left" }} >{player[0]}</div>
+                                            <div className="col-6" style={{ textAlign: "right" }} >{player[1]}</div>
 
 
                                         </div>
@@ -137,16 +159,16 @@ export class RatingLite extends Component {
                                 </div>
                             ))}
                         </div>
-                        
-                        <div className="gameList" style={{ textAlign: "center"}}>
+
+                        <div className="gameList" style={{ textAlign: "center" }}>
                             <h2 >Game History</h2>
                             {games.map(game => (
-                                <div >
+                                <div key={game.timestamp}>
                                     <div>
                                         <div className="row">
 
-                                            <div className="col-6" style={{textAlign:"left"}} >{game.winner}</div>
-                                            <div className="col-6" style={{textAlign:"right"}} >{game.loser}</div>
+                                            <div className="col-6" style={{ textAlign: "left" }} >{game.winner}</div>
+                                            <div className="col-6" style={{ textAlign: "right" }} >{game.loser}</div>
 
 
                                         </div>
